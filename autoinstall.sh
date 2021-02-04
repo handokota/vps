ed54f5fef3239bb1c85af7abb70c3d1c09f4efc9
handokota <64225391+handokota@users.noreply.github.com>
2021-02-04 14:08:27 +0700

    first commit

--- autoinstall.sh
+++ autoinstall.sh
@@ -29,14 +29,14 @@ apt-get -y install vnstat
 service vnstat restart
 
 # edit common-password
-mv /vps/common-password /etc/pam.d/common-password
+mv vps/common-password /etc/pam.d/common-password
 
 # install speedtest
 apt-get -y install speedtest-cli
 
 # letsencrypt
 #mkdir -p /etc/letsencrypt/live
-#mv /vps/sg01.oneserver.xyz /etc/letsencrypt/live/sg01.hostervpn.com
+#mv vps/sg01.oneserver.xyz /etc/letsencrypt/live/sg01.hostervpn.com
 
 # install autoreboot and autokill
 echo "0 0 * * * root /usr/bin/del-exp.sh" > /etc/crontab
@@ -62,29 +62,29 @@ echo "neofetch" >> .bash_profile
 
 # install nginx
 apt-get -y install nginx
-mv /vps/nginx/default /etc/nginx/sites-available/default
+mv vps/nginx/default /etc/nginx/sites-available/default
 mv /usr/share/nginx/html/index.html /usr/share/nginx/html/index.html.bak
 mkdir /usr/share/nginx/html/ovpn
 service nginx restart
 
 # install menu
-mv /vps/autokill.sh /usr/bin/autokill.sh
+mv vps/autokill.sh /usr/bin/autokill.sh
 chmod +x /usr/bin/autokill.sh
-mv /vps/autoreboot.sh /usr/bin/autoreboot.sh
+mv vps/autoreboot.sh /usr/bin/autoreboot.sh
 chmod +x /usr/bin/autoreboot.sh
-mv /vps/bash/menu /usr/bin/menu
+mv vps/bash/menu /usr/bin/menu
 chmod +x /usr/bin/menu
-mv /vps/bash/create.sh /usr/bin/create.sh
+mv vps/bash/create.sh /usr/bin/create.sh
 chmod +x /usr/bin/create.sh
-mv /vps/bash/del-exp.sh /usr/bin/del-exp.sh
+mv vps/bash/del-exp.sh /usr/bin/del-exp.sh
 chmod +x /usr/bin/del-exp.sh
-mv /vps/bash/user-delete.sh /usr/bin/user-delete.sh
+mv vps/bash/user-delete.sh /usr/bin/user-delete.sh
 chmod +x /usr/bin/user-delete.sh
-mv /vps/bash/menu /usr/bin/user-extend.sh
+mv vps/bash/menu /usr/bin/user-extend.sh
 chmod +x /usr/bin/user-extend.sh
-mv /vps/bash/user-login.sh /usr/bin/user-login.sh
+mv vps/bash/user-login.sh /usr/bin/user-login.sh
 chmod +x /usr/bin/user-login.sh
-mv /vps/bash/user-list.sh /usr/bin/user-list.sh
+mv vps/bash/user-list.sh /usr/bin/user-list.sh
 chmod +x /usr/bin/user-list.sh
 
 # install badvpn
@@ -92,19 +92,19 @@ apt-get -y install cmake
 tar xf badvpn-1.999.130.tar.gz
 mkdir badvpn-build
 cd badvpn-build
-cmake /vps/badvpn-1.999.130 -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
+cmake vps/badvpn-1.999.130 -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
 make install
 
 # install rc-local
 cd
-mv /vps/rc.local /etc/rc.local
+mv vps/rc.local /etc/rc.local
 chmod +x /etc/rc.local
-mv /vps/services/rc-local.service /etc/systemd/system/rc-local.service
+mv vps/services/rc-local.service /etc/systemd/system/rc-local.service
 systemctl enable rc-local.service
 service rc-local restart
 
 # install banner
-mv /vps/banner.html /etc/issue.net
+mv vps/banner.html /etc/issue.net
 sed -i 's@#Banner none@Banner /etc/issue.net@g' /etc/ssh/sshd_config
 service ssh restart
 
@@ -119,19 +119,19 @@ service dropbear restart
 
 # install stunnel4
 apt-get -y install stunnel4
-mv /vps/stunnel.conf /etc/stunnel/stunnel.conf
-mv /vps/stunnel.pem /etc/stunnel/stunnel.pem
+mv vps/stunnel.conf /etc/stunnel/stunnel.conf
+mv vps/stunnel.pem /etc/stunnel/stunnel.pem
 service stunnel4 restart
 
 # install squid
 apt-get -y install squid3
 mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
-mv /vps/squid.conf /etc/squid/squid.conf
+mv vps/squid.conf /etc/squid/squid.conf
 service squid restart
 
 # install sslh
 apt-get -y install sslh
-mv /vps/sslh /etc/default/sslh
+mv vps/sslh /etc/default/sslh
 service sslh restart
 
 # install webmin
@@ -154,8 +154,8 @@ service webmin restart
 
 # install shadowsocksr
 cd
-chmod +x /vps/app/shadowsocksR.sh
-cd /vps/app
+chmod +x vps/app/shadowsocksR.sh
+cd vps/app
 ./shadowsocksR.sh
 mkdir -p /etc/shadowsocks-r
 mv /etc/shadowsocks.json /etc/shadowsocks-r/config.json
@@ -163,19 +163,19 @@ service shadowsocks restart
 
 # install shadowsocks-libev
 apt-get -y install shadowsocks-libev
-mv /vps/shadowsocks/config.json /etc/shadowsocks-libev/config.json
+mv vps/shadowsocks/config.json /etc/shadowsocks-libev/config.json
 service shadowsocks-libev restart
 
 # install cloak
 cd
-chmod +x ~/app/Cloak-Installer.sh
-cd /vps/app
+chmod +x vps/app/Cloak-Installer.sh
+cd vps/app
 ./Cloak-Installer.sh
 cd
 wget https://github.com/cbeuw/Cloak/releases/download/v2.2.1/ck-server-linux-amd64-2.2.1
-mv /vps/ck-server-linux-amd64-2.2.1 /usr/bin/ck-server
+mv vps/ck-server-linux-amd64-2.2.1 /usr/bin/ck-server
 chmod +x /usr/bin/ck-server
-mv /vps/services/cloak.service /etc/systemd/system/cloak.service
+mv vps/services/cloak.service /etc/systemd/system/cloak.service
 systemctl enable cloak.service
 service cloak restart
 service cloak-server restart
@@ -194,38 +194,38 @@ make install
 cd
 wget https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.1/v2ray-plugin-linux-amd64-v1.3.1.tar.gz
 tar -zxvf v2ray-plugin-linux-amd64-v1.3.1.tar.gz
-mv /vps/v2ray-plugin_linux_amd64 /usr/bin/v2ray-plugin
+mv vps/v2ray-plugin_linux_amd64 /usr/bin/v2ray-plugin
 chmod +x /usr/bin/v2ray-plugin
 
 # obfs-tls
 cd
-mv /vps/shadowsocks/obfs-tls.json /etc/shadowsocks-libev/obfs-tls.json
-mv /vps/services/obfs-tls.service /etc/systemd/system/obfs-tls.service
+mv vps/shadowsocks/obfs-tls.json /etc/shadowsocks-libev/obfs-tls.json
+mv vps/services/obfs-tls.service /etc/systemd/system/obfs-tls.service
 systemctl enable obfs-tls.service
 service obfs-tls restart
 
 # obfs-http
-mv /vps/shadowsocks/obfs-http.json /etc/shadowsocks-libev/obfs-http.json
-mv /vps/services/obfs-http.service /etc/systemd/system/obfs-http.service
+mv vps/shadowsocks/obfs-http.json /etc/shadowsocks-libev/obfs-http.json
+mv vps/services/obfs-http.service /etc/systemd/system/obfs-http.service
 systemctl enable obfs-http.service
 service obfs-http restart
 
 # ss-v2ray
-mv /vps/shadowsocks/v2ray.json /etc/shadowsocks-libev/v2ray.json
-mv /vps/services/ss-v2ray.service /etc/systemd/system/ss-v2ray.service
+mv vps/shadowsocks/v2ray.json /etc/shadowsocks-libev/v2ray.json
+mv vps/services/ss-v2ray.service /etc/systemd/system/ss-v2ray.service
 systemctl enable ss-v2ray.service
 service ss-v2ray restart
 
 # install openvpn
 cd
-chmod +x /vps/app/openvpn-install.sh
-cd /vps/app
+chmod +x vps/app/openvpn-install.sh
+cd vps/app
 ./openvpn-install.sh
 
 #install wireguard
 cd
-chmod +x ~/app/wireguard-install.sh
-cd /vps/app
+chmod +x vps/app/wireguard-install.sh
+cd vps/app
 ./wireguard-install.sh
 
 # install trojan
